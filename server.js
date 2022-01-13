7ad0f8d721747094b4a4b4b94809a83019ecc6ca
phhuyhoang <phhuyhoang168@gmail.com>
2022-01-14 02:13:02 +0700

    app: update express server setup

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
 const fs = require('fs');
 const path = require('path');
+const hbs = require('hbs');
 const dotenv = require('dotenv');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const config = require('./config');
+const env = config.env;
 const ExpressHelper = require('./helpers/server/express');
+const HandlebarsHelper = require('./helpers/util/HandlebarsHelper');
 
+const port = process.env.PORT || 8080;
 const server = express();
-
 
 // --------------------------------------------------------------------
 //  Configuration
 // --------------------------------------------------------------------
+server.locals.hbs = hbs
+
+// Public
 server.use(express.static('public'));
-
+// Views
+server.engine('hbs', hbs.__express);
+server.set('views', env.parsed.ENTRY_VIEWS);
+server.set('view engine', 'hbs');
+ExpressHelper.autoloadViews(server, env.parsed.ENTRY_VIEWS, [ 'partials' ])
+// Routes
 ExpressHelper.autoloadRoutes(server, config.env.parsed.ENTRY_ROUTES);
+// Helpers
+HandlebarsHelper.loadAllHelpers(hbs);
+// Middlewares
+server.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-const listener = server.listen(8080, function onStartListen() {
+const listener = server.listen(port, function onStartListen() {
   console.log("Listening on port %d", listener.address().port);
 });
 
