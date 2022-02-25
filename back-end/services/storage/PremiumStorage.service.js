090c952942d7e1b976557669ff62d59f9ad9d819
phhuyhoang <phhuyhoang168@gmail.com>
2022-02-25 17:10:16 +0700

    service: implement storage services
    Two different storage benefits, exactly as requirement required.

---
+++ back-end/services/storage/PremiumStorage.service.js
@@ -0,0 +1,48 @@
+const mime = require('mime');
+const crypto = require('crypto');
+const multer = require('multer');
+
+const c_static = require('../../constants/static.constants');
+const syslib = require('../../helpers/SysLib.helper');
+
+
+module.exports = function PremiumStorage() {
+  const storage = multer.diskStorage({
+    // Create a hash string based on timestamp and original filename.
+    // Use to unique-ization folder name that storing uploaded file from user.
+    destination: function encryptFolderBaseOnDate(request, file, confirm) {
+      const stringRequestHeaders = JSON.stringify(request.headers || {})
+      const uniqueHashString = crypto.createHash('sha1')
+        .update(
+          new Date().toString().concat(stringRequestHeaders, file.originalname), 'binary')
+        .digest('hex');
+
+      const explicitDestination = syslib.path.resolveThenCreateFolder(c_static.PUBLIC_UPLOAD_DESTINATION, uniqueHashString);
+
+      confirm(null, explicitDestination);
+    },
+
+    // Because the parent folder name is already unique, the uploaded file inside will have a common name.
+    filename: function addMimeExtension(request, file, confirm) {
+      const mimetype = file.mimetype;
+      const extension = mime.getExtension(mimetype);
+
+      const temporaryFilename = `file.${extension}`
+      confirm(null, temporaryFilename);
+    },
+
+    // Just be against invalid files sent to the server
+    fileFilter: function acceptPDFDocumentOnly(request, file, confirm) {
+      const isValidPDFMimeType = ['application/pdf', 'application/x-pdf'].includes(file.mimetype);
+
+      if (isValidPDFMimeType) {
+        confirm(null, true); // Accept case
+      }
+      else {
+        confirm(null, false); // Reject case
+      }
+    }
+  });
+
+  return multer({ storage });
+}
