b473a75624c3b09c17e0ed15c1418792a405c449
phhuyhoang <phhuyhoang168@gmail.com>
2022-02-25 16:12:18 +0700

    refactor: delete anbundant helpers and restructure

---
+++ back-end/helpers/Express.helper.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const determiner = require('./Determiner.helper');
+
+
+/**
+ * Help server.js look more concise and readable
+ */
+class ExpressHelper {
+
+  /**
+   * Load all routes from the given directory. 
+   * In case the route name is `index`, automatically load to root (`/`).
+   * @param {Express} server - The Express application server
+   * @param {string} directory - A folder contains routers
+   */
+  static autoloadRoutes(server, directory) {
+    for (const file of fs.readdirSync(directory)) {
+      const route = {
+        name: path.parse(file).name,
+        file: path.resolve(`${directory}/${file}`)
+      }
+      
+      route.name == 'index' 
+        ? server.use('/', require(route.file))
+        : server.use(`/${route.name}`, require(route.file))
+    }
+  }
+
+  /**
+   * @param {Express} server
+   * @param {string} root
+   * @param {string[]} folders
+   */
+  static autoloadViews(server, root, folders) {
+    for (const folder of folders) {
+      const handlebarFiles = glob.sync(`${root}/${folder}/**/*.hbs`);
+      for (const handlebarFile of handlebarFiles) {
+        const partialName = path.parse(handlebarFile).name;
+        // console.log(1)
+        server.locals.hbs.registerPartial(partialName, fs.readFileSync(handlebarFile, 'utf-8'));
+      }
+    }
+  }
+
+
+  static autoloadMiddlewares(server, folders) {
+    const middlewareFiles = glob.sync(path.resolve(folders, '**/*.middleware.js'), { absolute: true });
+    for (const file of middlewareFiles) {
+      const middleware = require(file);
+
+      if (determiner.isClass(middleware) && determiner.isFunction(middleware.handle)) {
+        server.use(middleware.handle);
+      }
+      else if (determiner.isPureFunction(middleware)) {
+        server.use(middleware);
+      }
+    }
+  }
+}
+
+module.exports = ExpressHelper;
--- back-end/helpers/util/HandlebarsHelper.js
+++
@@ -1,8 +0,0 @@
-const helpers = [];
-
-
-module.exports.loadAllHelpers = function loadAllHelpers(hbs) {
-  for (const helper of helpers) {
-    hbs.registerHelper(helper.name, helper);
-  }
-}
