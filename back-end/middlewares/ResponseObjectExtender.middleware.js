46559afe0d867e990d972f38bb3ecc99d498c324
phhuyhoang <phhuyhoang168@gmail.com>
2022-02-25 17:01:47 +0700

    middleware: add middleware
    This middleware codestyle is inspired by Laravel middleware.
    It's only serves to extend request/response and reuse them
    more succinctly.

---
+++ back-end/middlewares/ResponseObjectExtender.middleware.js
@@ -0,0 +1,40 @@
+const _ = require('lodash');
+
+
+class ResponseObjectExtender {
+
+  static plugs = {
+    /**
+     * Helps to send a pseudo error to client. It provides a flexible way to 
+     * show a custom error message from server.
+     * 
+     */
+    sendErrorAsJSON: function sendErrorAsJSON(error, code, message) {
+      const errorPacket = {
+        error: {
+          name: error.name,
+          message: message || error.message,
+          code,
+        }
+      };
+
+      return Promise.resolve(errorPacket).then(() => response.json(errorPacket));
+    }
+  }
+  
+  static handle(request, response, next) {
+
+    const self = ResponseObjectExtender;
+
+    if (!_.chain(response).has(_.keys(self.plugs)).value()) {
+      _.assign(response, self.plugs);
+    }
+
+    next();
+
+  }
+
+}
+
+
+module.exports = ResponseObjectExtender;
