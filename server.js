eefff6674cdc75f34bda992c4fddea398a2cf855
phhuyhoang <phhuyhoang168@gmail.com>
2022-03-15 15:37:16 +0700

    chore: update depedencies

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const hbs = require('hbs');
 const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
+const userAgent = require('express-useragent');
 
 const config = require('./back-end/configs/');
 const ExpressHelpers = require('./back-end/helpers/Express.helper');
-const HandlebarHelpers = require('handlebars-helpers')({ handlebars: hbs });
+const HandlebarHelpers = require('./back-end/helpers/Handlebars.helper')({ handlebars: hbs });
 
 
 const port = process.env.PORT || 8080;
@@ -18,7 +19,7 @@ const server = express();
 //  Configuration
 // --------------------------------------------------------------------
 server.locals.hbs = hbs
-
+server.enable('trust proxy', true);
 
 // --------------------------- Public ---------------------------
 server.use(express.static('./front-end/public'));
@@ -29,12 +30,14 @@ server.use(express.static('./front-end/public'));
 server.engine('hbs', hbs.__express);
 server.set('views', config.env.parsed.ENTRY_VIEWS);
 server.set('view engine', 'hbs');
+server.set('etag', 'strong');
 ExpressHelpers.autoloadViews(server, config.env.parsed.ENTRY_VIEWS, [ 'partials' ])
 
 
 // --------------------------- Middlewares ---------------------------
 server.use(express.json());
 server.use(bodyParser.urlencoded({ extended: true }));
+server.use(userAgent.express());
 ExpressHelpers.autoloadMiddlewares(server, config.env.parsed.ENTRY_MIDDLEWARES);
 
 
