ec0f95b64d85c1d9152a144d211e7f31fe3fbeef
phhuyhoang <phhuyhoang168@gmail.com>
2022-03-23 14:41:34 +0700

    route: done register route with secured
    Limit requests per second to prevent API attack

--- back-end/routes/register.js
+++ back-end/routes/register.js
@@ -1,11 +1,41 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+const datetools = require("date-fns");
 const router = express.Router();
 
 const RegisterController = require('../controllers/auth/Register.controller');
 
 
-// Register route
+const ResendCodeRequestLimiter = rateLimit(
+  {
+    windowMs: datetools.minutesToMilliseconds(1),
+    max: 2,
+  }
+);
+
+
+const VerificationCodeRequestLimiter = rateLimit(
+  {
+    windowMs: datetools.secondsToMilliseconds(1),
+    max: 1,  
+  }
+);
+
+
+const RegisterSubmitRequestLimiter = rateLimit(
+  {
+    windowMs: datetools.secondsToMilliseconds(1),
+    max: 1, 
+  }
+);
+
+
+// Register page
 router.get('/', (req, res, next) => res.render('register'));
+
+
+// Register success congratulations page
+router.get('/success', (req, res, next) => res.render('register-success'));
 
 
 // Validate form part-by-part
@@ -13,15 +43,15 @@ router.post('/validate', RegisterController.validateForm);
 
 
 // Request a verification code
-router.post('/code', RegisterController.sendCodeToEmail);
+router.post('/code', VerificationCodeRequestLimiter, RegisterController.sendCodeToEmail);
 
 
 // Request resend verification code
-router.post('/resend', RegisterController.resendVerificationCode);
+router.post('/resend', ResendCodeRequestLimiter, RegisterController.resendVerificationCode);
 
 
-// Submit register form, verification code then save to database
-router.post('/submit', RegisterController.createAccount);
+// Submit register form, check verification code then save to database
+router.post('/submit', RegisterSubmitRequestLimiter, RegisterController.createAccount);
 
 
 module.exports = router;
